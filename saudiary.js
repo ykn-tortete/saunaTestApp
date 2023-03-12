1ef30ebb33dcf65fef84ed3eb5e75e7c00c5efb0
ykn-tortete <81640247+ykn-tortete@users.noreply.github.com>
2023-03-13 00:05:12 +0900

    Add files via upload

--- saudiary.js
+++ saudiary.js
@@ -6,68 +6,73 @@
     projectId: 'sauna-test-app',
     });
   
-    const db = firebase.firestore();
+  const db = firebase.firestore();
 
-    // document.querySelector('#get').addEventListener('click', function() {
-      db.collection('saulog')
-        .get()
-        .then(querySnapshot => {
-          querySnapshot.forEach(doc => {
+  db.collection('saulog')
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
 
-            // const dataJson = JSON.stringify(doc.data())
-            // document.querySelector('td.no').textContent = index;
+        let row = document.createElement('tr');
 
-            let row = document.createElement('tr');
+        let cellSet = document.createElement('td');
+        let cellSetText = document.createTextNode(doc.data().set);
+        cellSet.appendChild(cellSetText);
 
-            let cellSaunaTemp = document.createElement('td');
-            let cellSaunaTempText = document.createTextNode(doc.data().sauna_temp);
-            cellSaunaTemp.appendChild(cellSaunaTempText);
+        let cellSaunaTemp = document.createElement('td');
+        let cellSaunaTempText = document.createTextNode(doc.data().sauna_temp);
+        cellSaunaTemp.appendChild(cellSaunaTempText);
 
-            let cellSaunaTime = document.createElement('td');
-            let cellSaunaTimeText = document.createTextNode(doc.data().sauna_time);
-            cellSaunaTime.appendChild(cellSaunaTimeText);
+        let cellSaunaTime = document.createElement('td');
+        let cellSaunaTimeText = document.createTextNode(doc.data().sauna_time);
+        cellSaunaTime.appendChild(cellSaunaTimeText);
 
-            let cellSaunaHr = document.createElement('td');
-            let cellSaunaHrText = document.createTextNode(doc.data().sauna_hr);
-            cellSaunaHr.appendChild(cellSaunaHrText);
+        let cellSaunaHr = document.createElement('td');
+        let cellSaunaHrText = document.createTextNode(doc.data().sauna_hr);
+        cellSaunaHr.appendChild(cellSaunaHrText);
 
 
-            let cellBathTemp = document.createElement('td');
-            let cellBathTempText = document.createTextNode(doc.data().bath_temp);
-            cellBathTemp.appendChild(cellBathTempText);
-            
-            let cellBathTime = document.createElement('td');
-            let cellBathTimeText = document.createTextNode(doc.data().bath_time);
-            cellBathTime.appendChild(cellBathTimeText);
+        let cellBathTemp = document.createElement('td');
+        let cellBathTempText = document.createTextNode(doc.data().bath_temp);
+        cellBathTemp.appendChild(cellBathTempText);
+        
+        let cellBathTime = document.createElement('td');
+        let cellBathTimeText = document.createTextNode(doc.data().bath_time);
+        cellBathTime.appendChild(cellBathTimeText);
 
-            let cellScore = document.createElement('td');
-            let cellScoreText = document.createTextNode(doc.data().score);
-            cellScore.appendChild(cellScoreText);
+        let cellScore = document.createElement('td');
+        let cellScoreText = document.createTextNode(doc.data().score);
+        cellScore.appendChild(cellScoreText);
 
-            row.appendChild(cellSaunaTemp);
-            row.appendChild(cellSaunaTime);
-            row.appendChild(cellSaunaHr);
-            row.appendChild(cellBathTemp);
-            row.appendChild(cellBathTime);
-            row.appendChild(cellScore);
+        row.appendChild(cellSet);
+        row.appendChild(cellSaunaTemp);
+        row.appendChild(cellSaunaTime);
+        row.appendChild(cellSaunaHr);
+        row.appendChild(cellBathTemp);
+        row.appendChild(cellBathTime);
+        row.appendChild(cellScore);
 
-            document.querySelector('tbody').appendChild(row);
-            });
-            
-            // document.querySelector('td.sauna-temp').textContent = doc.data().sauna_temp;
-            // document.querySelector('td.sauna-time').textContent = doc.data().sauna_time;
-            // document.querySelector('td.sauna-hr').textContent = doc.data().sauna_hr;
-            // document.querySelector('td.bath-temp').textContent = doc.data().sauna_temp;
-            // document.querySelector('td.bath-time').textContent = doc.data().sauna_temp;
-            // document.querySelector('td.score').textContent = doc.data().sauna_temp;
-            
+        let visitRef = db.collection('visit');
+        let visitIdRef = visitRef.doc(doc.data().visitID)
+        visitIdRef.get().then(docSnapshot => {
 
-            // const p = document.createElement('p');
-            // const result = document.createTextNode(
-            //   `${doc.id} => ${JSON.stringify(doc.data())}`,
-            // );
-            // p.appendChild(result);
-            // document.body.appendChild(p);
+          let cellDate = document.createElement('td');
+          let cellDateText = document.createTextNode(docSnapshot.data().date);
+          cellDate.appendChild(cellDateText);
+          row.appendChild(cellDate);
+
+          let cellFacility = document.createElement('td');
+          let cellFacilityText = document.createTextNode(docSnapshot.data().facility);
+          cellFacility.appendChild(cellFacilityText);
+          row.appendChild(cellFacility);
+
+          let cellPref = document.createElement('td');
+          let cellPrefText = document.createTextNode(docSnapshot.data().prefecture);
+          cellPref.appendChild(cellPrefText);
+          row.appendChild(cellPref);
         });
-    // });
+
+        document.querySelector('tbody').appendChild(row);
+      });
+    });
 }
