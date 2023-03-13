bfc4a5fcbe2fd6f12992100f3409b94633e6f605
ykn-tortete <81640247+ykn-tortete@users.noreply.github.com>
2023-03-13 21:12:14 +0900

    Add files via upload

--- js/saulog.js
+++ js/saulog.js
@@ -7,6 +7,61 @@
   });
 
   const db = firebase.firestore();
+
+  // 施設のoptionを追加
+  db.collection('facility')
+  .get()
+  .then(querySnapshot => {
+    querySnapshot.forEach(doc => {
+
+      let option = document.createElement('option');
+
+      let optionText = document.createTextNode(doc.data().name);
+      option.appendChild(optionText);
+
+      document.getElementById('facility').appendChild(option);
+    });
+  });
+
+  // セット数に応じて表示する入力領域を変動
+  set.addEventListener('change', () => {
+    //入力フォームのリセット
+    if (document.getElementsByClassName('loop').length) {
+      const loop = document.querySelector('.loop');
+      loop.remove();
+    }
+    //送信ボタンのリセット
+    if (document.getElementsByClassName('button2').length) {
+      const btn = document.querySelector('.button2');
+      btn.remove();
+    }
+
+    const set = document.getElementById('set');
+    const setValue = set.value;
+
+    if (setValue > 0) {
+      let tmplNode = document.querySelector('div.tmpl-node');
+      let mergeElement = document.createElement('div');
+      mergeElement.className = 'loop'
+      for (let i=0; i<setValue; i++) {
+        let newNode = tmplNode.cloneNode(true);
+        newNode.style.display ='block';
+        //idは正しく入ってる？？
+        newNode.id = 'box' + i;
+        //セット数を変更
+        newNode.firstElementChild.innerText = `${i + 1}セット`;
+        mergeElement.appendChild(newNode);
+      }
+      let diaryNode = document.querySelector('.diary');
+      diaryNode.append(mergeElement);
+
+      const btnNode = document.querySelector('button');
+      const newBtnNode = btnNode.cloneNode(true);
+      newBtnNode.style.display = 'block';
+      newBtnNode.className = 'button2';
+      diaryNode.append(newBtnNode);
+    }
+  });
   
   document.querySelector('button').addEventListener('click', () => {
     db.collection('saulog')
